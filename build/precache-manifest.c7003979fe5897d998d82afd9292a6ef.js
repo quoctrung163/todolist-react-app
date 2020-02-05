b065ca113148b0ef2907f59b08762b0f7a808e88
quoctrung163 <miyucoder@gmail.com>
2020-02-05 09:07:10 +0700

    buid production

---
+++ build/precache-manifest.c7003979fe5897d998d82afd9292a6ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91dbe19e1dc10c66b655e3a6fc39c7de",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "0ac12f4e6c85eb921b7a",
+    "url": "/react-tutorial/static/css/main.59093e61.chunk.css"
+  },
+  {
+    "revision": "9ba17663be0ccc30c2f3",
+    "url": "/react-tutorial/static/js/2.19d8c4b9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tutorial/static/js/2.19d8c4b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac12f4e6c85eb921b7a",
+    "url": "/react-tutorial/static/js/main.f5ceab60.chunk.js"
+  },
+  {
+    "revision": "fa6d4ce3154e8ce2df3e",
+    "url": "/react-tutorial/static/js/runtime-main.36324acb.js"
+  }
+]);
