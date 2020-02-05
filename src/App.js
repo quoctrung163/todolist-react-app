df25614c9ab3153ef24977941a2431ad523319a5
quoctrung163 <miyucoder@gmail.com>
2020-02-05 07:53:42 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,41 +4,41 @@ import Form from "./Form";
 import "./styles.css";
 
 class App extends Component {
-  state = {
-    characters: []
-  };
+   state = {
+      characters: []
+   };
 
-  removeCharacter = index => {
-    const { characters } = this.state;
+   removeCharacter = index => {
+      const { characters } = this.state;
 
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index;
-      })
-    });
-  };
+      this.setState({
+         characters: characters.filter((character, i) => {
+            return i !== index;
+         })
+      });
+   };
 
-  handleSubmit = character => {
-    this.setState({
-      characters: [...this.state.characters, character]
-    });
-  };
+   handleSubmit = character => {
+      this.setState({
+         characters: [...this.state.characters, character]
+      });
+   };
 
-  render() {
-    const { characters } = this.state;
+   render() {
+      const { characters } = this.state;
 
-    return (
+      return (
       <div className="container">
-        <h1>Example ReactJS</h1>
-        <p>Add a character with a name and a job to the table.</p>
-        <Table
-          characterData={characters}
-          removeCharacter={this.removeCharacter}
-        />
-        <Form handleSubmit={this.handleSubmit} />
+         <h1>Example ReactJS</h1>
+         <p>Add a character with a name and a job to the table.</p>
+         <Table
+            characterData={characters}
+            removeCharacter={this.removeCharacter}
+         />
+         <Form handleSubmit={this.handleSubmit} />
       </div>
-    );
-  }
+      );
+   }
 }
 
 export default App;
